bef24ffa19a0b414e3154a2773effd3ed3f92f5b
Felix Kugler <relgukxilef@gmail.com>
2025-05-13 02:31:42 +0200

    Implement LEB128 code

--- include/bnp/bnp.h
+++ include/bnp/bnp.h
@@ -1,7 +1,25 @@
 #pragma once
 
-void test();
+#include <cstdint>
+#include <vector>
 
 namespace bnp {
-    
+    struct id {
+        std::uint8_t data[16];
+    };
+
+    struct field {
+        id id;
+        enum {
+            fixed, dynamic
+        } primitive_type;
+        unsigned dynamic_size;
+    };
+
+    struct view {
+        std::uint8_t *begin, *end;
+    };
+
+    unsigned leb128(view bytes);
+    void leb128(unsigned value, view &bytes);
 }
--- source/bnp/bnp.cpp
+++ source/bnp/bnp.cpp
@@ -1,5 +1,36 @@
 #include <bnp/bnp.h>
 
-void test() {
+using namespace bnp;
+using namespace std;
 
+uint8_t byte(view &bytes) {
+    uint8_t value = *bytes.begin;
+    bytes.begin = min(bytes.begin + 1, bytes.end);
+    return value;
+}
+
+void byte(uint8_t value, view &bytes) {
+    *bytes.begin = value;
+    bytes.begin = min(bytes.begin + 1, bytes.end);
+}
+
+unsigned leb128(view &bytes) {
+    auto sum = 0u;
+    auto shift = 0u;
+    do {
+        auto value = byte(bytes);
+        sum += (value & 0x7f) << shift;
+        shift += 7;
+    } while (sum >= 128);
+    return sum;
+}
+
+void leb128(unsigned value, view &bytes) {
+    do {
+        auto next = value & 0x7f;
+        value >>= 7;
+        if (value > 0)
+            next |= 0x80;
+        byte(next, bytes);
+    } while (value > 0);
 }
